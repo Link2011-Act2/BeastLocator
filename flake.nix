7fdf273c513c10dd4d1d33baffe9c1d28aeff034
spring <harusan@spring-server.com>
2026-03-24 14:08:31 +0900

    Added flake.nix and some changes.

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Deno environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            deno
+	    pnpm
+	    pnpm-shell-completion
+          ];
+        };
+      }
+    );
+}
