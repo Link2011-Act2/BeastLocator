a4df173cf2cda30be742e1e140f46596605509fd
spring <harusan@spring-server.com>
2026-03-27 22:00:58 +0900

    Edited settings.tsx, memo: いい感じの到着通知とかの文章とかトグルの永続化、反映に関する処理を書く

--- web/app/routes/settings.tsx
+++ web/app/routes/settings.tsx
@@ -2,15 +2,20 @@ import { useNavigate } from "react-router";
 
 export default function Settings() {
   const navigate = useNavigate();
-
+  const lat = 35.665544
+  const lng = 139.6699717
   return (
     <main style={{ paddingTop: "4rem" }}>
       <div style={{ position: "fixed", top: "1rem", left: "1rem" }}>
         <button className="circle" onClick={() => navigate(-1)}>
           <i>arrow_back</i>
         </button>
-      </div>
-      <h5>設定</h5>
+	<h5>設定</h5>
+	<h6>現在の目的地</h6>
+	<p>緯度: {lat} / 経度: {lng}</p> 
+	<h6>到着通知</h6>
+	<p>いい感じの到着通知とかを設定する文章とトグルと数値をlocalstorageに保管する処理を書く</p>
+      </div> 
     </main>
   );
 }
